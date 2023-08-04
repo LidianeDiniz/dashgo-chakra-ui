60a633e99f3c785aafb841594067606fa1cbad7d
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-08-04 16:33:55 -0300

    Página: Listagem de usuário

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,241 @@
+import { Header } from "@/components/Form/Header";
+import { Sidebar } from "@/components/Form/Sidebar";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Text,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" marginY="6" maxWidth={1480} marginX="auto" paddingX="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" padding="8">
+          <Flex marginBottom="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize={20} />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th paddingX="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Lidiane Diniz</Text>
+                    <Text fontSize="small" color="gray.300">
+                      lidiane@email.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>03 de Agosto, 2023</Td>
+
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Lidiane Diniz</Text>
+                    <Text fontSize="small" color="gray.300">
+                      lidiane@email.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>03 de Agosto, 2023</Td>
+
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Lidiane Diniz</Text>
+                    <Text fontSize="small" color="gray.300">
+                      lidiane@email.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>03 de Agosto, 2023</Td>
+
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Lidiane Diniz</Text>
+                    <Text fontSize="small" color="gray.300">
+                      lidiane@email.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>03 de Agosto, 2023</Td>
+
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Lidiane Diniz</Text>
+                    <Text fontSize="small" color="gray.300">
+                      lidiane@email.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>03 de Agosto, 2023</Td>
+
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td paddingX="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Lidiane Diniz</Text>
+                    <Text fontSize="small" color="gray.300">
+                      lidiane@email.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                <Td>03 de Agosto, 2023</Td>
+
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="green"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
